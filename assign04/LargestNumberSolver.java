d3d55ef8f0b46e03566039a2de3ffa4561ab10d2
Dremich <adtolton@gmail.com>
2023-02-07 19:08:59 -0700

    Initial testing

--- assign04/LargestNumberSolver.java
+++ assign04/LargestNumberSolver.java
@@ -1,5 +1,40 @@
 package assign04;
 
-public class LargestNumberSolver {
+import java.math.BigInteger;
+import java.util.Comparator;
+import java.util.List;
 
+public class LargestNumberSolver {
+	
+	public static <T> void insertionSort(T[] arr, Comparator<? super T> cmp) {
+		
+	}
+	
+	public static BigInteger findLargestNumber(Integer[] arr) {
+		
+		return null;
+	}
+	
+	public static int findLargestInt(Integer[] arr) throws OutOfRangeException {
+		
+		// Call BigInt version
+		
+		return null;
+	}
+	
+	public static long findLargestLong(Integer[] arr) throws OutOfRangeException {
+		
+	}
+	
+	public static BigInteger sum(List<Integer[]> list) {
+		
+	}
+
+	public static Integer[] findKthLargest(List<Integer[]> list, int k) throws IllegalArgumentException {
+		
+	}
+	
+	public static List<Integer[]> readFile(String filename) {
+		
+	}
 }
--- assign04/LargestNumberSolverTest.java
+++ assign04/LargestNumberSolverTest.java
@@ -1,5 +1,78 @@
 package assign04;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+
+
 public class LargestNumberSolverTest {
 
+	@BeforeEach
+	void setup() {
+		Integer[] arr1 = {1, 0, 2};
+		Integer[] arr2 = {15, 25, 73, 3};
+	}
+	
+	// Basic Tests
+	
+	@Test
+	void basicInsertionSort() {
+		assertTrue(Arrays.equals(arr1, new Integer[] {1, 0, 2}));
+	}
+	
+	@Test
+	void basicFindLargestNumber() {
+		
+	}
+	
+	@Test
+	void basicFindLargestInt() {
+		
+	}
+	
+	@Test
+	void basicFindLargestLong() {
+		
+	}
+	
+	@Test 
+	void basicSum() {
+		
+	}
+	
+	@Test 
+	void basicKthLargest() {
+		
+	}
+	
+	@Test
+	void basicReadFile() {
+		
+	}
+	
+	// Exception Tests
+	
+	@Test
+	void findLargestIntException() {
+		
+	}
+	
+	@Test
+	void findLargestLongException() {
+		
+	}
+	
+	@Test
+	void noFindLargestNumberException() {
+		
+	}
+	
+	@Test
+	void kthLargestIllegalArgument() {
+		
+	}
+	
+	
+	
 }
