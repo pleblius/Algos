70f8ab5a32f087b560b5bdbc2574ad30453745f9
Dremich <adtolton@gmail.com>
2023-02-26 22:08:13 -0700

    penultimate implementation?

--- assign06/LinkedListStack.java
+++ assign06/LinkedListStack.java
@@ -29,20 +29,21 @@ public class LinkedListStack<E> implements Stack<E> {
 
 	@Override
 	public E pop() throws NoSuchElementException {
-		// TODO Auto-generated method stub
-		return null;
+		if (this.isEmpty()) {
+			throw new NoSuchElementException("There are no items in the stack.");
+		}
+		
+		return stack.deleteFirst();
 	}
 
 	@Override
 	public void push(E element) {
-		// TODO Auto-generated method stub
-		
+		stack.insertFirst(element);		
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return stack.size();
 	}
 
 }
--- assign06/WebBrowser.java
+++ assign06/WebBrowser.java
@@ -1,5 +1,56 @@
 package assign06;
 
-public class WebBrowser {
+import java.net.URL;
+import java.util.NoSuchElementException;
 
+public class WebBrowser {
+	
+	private ArrayStack<URL> forStack;
+	private ArrayStack<URL> backStack;
+	private URL currentPage;
+	
+	public WebBrowser() {
+		forStack = new ArrayStack<URL>();
+		backStack = new ArrayStack<URL>();
+	}
+	
+	public WebBrowser(SinglyLinkedList<URL> history) {
+		forStack = new ArrayStack<URL>();
+		
+		URL[] tempArr = (URL[]) history.toArray();
+		for (int ii = tempArr.length - 1; ii > 0; ii--) {
+			backStack.push(tempArr[ii]);
+		}
+		
+		currentPage = tempArr[0];
+	}
+	
+	public void visit(URL webpage) {
+		backStack.push(currentPage);
+		forStack.clear();
+		currentPage = webpage;
+	}
+	
+	public URL back() throws NoSuchElementException {
+		if (backStack.isEmpty()) {
+			throw new NoSuchElementException("Nowhere to go back to...");
+		}
+		
+		currentPage = backStack.pop();
+		return currentPage;
+	}
+	
+	public URL forward() throws NoSuchElementException {
+		if (forStack.isEmpty()) {
+			throw new NoSuchElementException("Nowhere to go...");
+		}
+		
+		backStack.push(currentPage);
+		currentPage = forStack.pop();
+		return currentPage;
+	}
+	
+	public SinglyLinkedList<URL> history() {
+		
+	}
 }
