7e161bd2f25ee17744c72a6d45e720e0bf4a9f3c
pleblius <pleblius@gmail.com>
2023-02-27 22:57:34 -0700

    Added Javadoc

--- assign06/LinkedListStack.java
+++ assign06/LinkedListStack.java
@@ -2,23 +2,56 @@ package assign06;
 
 import java.util.NoSuchElementException;
 
+/**
+ * This class represents a generic stack that utilizes a linked list for its ordering
+ * and operations. A stack is a last-in first-out (LIFO) data structure, and all
+ * operations can only be conducted on items that are "on top" of the stack--meaning
+ * the most-recent addition to the stack.
+ * 
+ * Because of implementation details for the linked list that this stack utilizes,
+ * all stack operations run in O(1) time.
+ * 
+ * @author Tyler Wilcox and Andrew Tolton.
+ * @version 28 February, 2023.
+ * 
+ * @param <E> - Generic type parameter of the elements contained in the stack.
+ */
 public class LinkedListStack<E> implements Stack<E> {
 	SinglyLinkedList<E> stack;
 	
+	/**
+	 * Creates a new, empty LinkedListStack object.
+	 */
 	public LinkedListStack() {
 		stack = new SinglyLinkedList<E>();
 	}
 	
+	/**
+	 * Removes all data elements from the stack.
+	 */
 	@Override
 	public void clear() {
-		stack = new SinglyLinkedList<E>();
+		stack.clear();
 	}
-
+	
+	/**
+	 * Checks if the stack is currently empty, containing no data elements.
+	 * 
+	 * @return true if the stack is empty, false otherwise.
+	 */
 	@Override
 	public boolean isEmpty() {
 		return stack.isEmpty();
 	}
 
+	/**
+	 * Gets the data element from the top item on the stack without removing it
+	 * from the stack.
+	 * Throws an exception if the stack is empty.
+	 * 
+	 * @return the data element contained at the top of the stack.
+	 * @throws NoSuchElementException - if the stack is empty.
+	 */
 	@Override
 	public E peek() throws NoSuchElementException {
 		if (this.isEmpty()) {
@@ -27,6 +60,14 @@ public class LinkedListStack<E> implements Stack<E> {
 		return stack.getFirst();
 	}
 
+	/**
+	 * Gets the data element from the top item on the stack and removes it
+	 * from the stack.
+	 * Throws an exception if the stack is empty.
+	 * 
+	 * @return the data element contained at the top of the stack.
+	 * @throws NoSuchElementException - if the stack is empty.
+	 */
 	@Override
 	public E pop() throws NoSuchElementException {
 		if (this.isEmpty()) {
@@ -36,11 +77,21 @@ public class LinkedListStack<E> implements Stack<E> {
 		return stack.deleteFirst();
 	}
 
+	/**
+	 * Places the given element on top of the stack.
+	 * 
+	 * @param element - the data element to be placed on the stack.
+	 */
 	@Override
 	public void push(E element) {
 		stack.insertFirst(element);		
 	}
 
+	/**
+	 * Gets the number of items currently on the stack.
+	 * 
+	 * @return the size of the stack.
+	 */
 	@Override
 	public int size() {
 		return stack.size();
--- assign06/SinglyLinkedList.java
+++ assign06/SinglyLinkedList.java
@@ -3,6 +3,22 @@ package assign06;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ * This class represents a singly-linked list of ordered objects of generic type.
+ * Data elements are tracked and ordered by the use of generic nodes, which contain
+ * a single pointer to the next node in the list.
+ * 
+ * Utilizes header and trailer structural nodes which contain no data elements.
+ * 
+ * Random access operations, including insertion, get, and delete methods
+ * at random points in the list are done in O(N) time.
+ * Operations on items at the front of the list are done in O(1) time.
+ * 
+ * @author Tyler Wilcox and Andrew Tolton
+ * @version 28 February, 2023
+ * 
+ * @param <E> Generic data type to be stored in the list
+ */
 public class SinglyLinkedList<E> implements List<E> {
 
 	Node<E> headerer;
@@ -10,6 +26,9 @@ public class SinglyLinkedList<E> implements List<E> {
 	Node<E> trailer;
 	int size;
 	
+	/**
+	 * Contructs a new, empty, generic SinglyLinkedList object.
+	 */
 	public SinglyLinkedList() {
 		trailer = new Node<E>(null, null);
 		header = new Node<E>(null, trailer);
@@ -18,6 +37,12 @@ public class SinglyLinkedList<E> implements List<E> {
 		size = 0;
 	}
 	
+	/**
+	 * Inserts the provided data element into the first node in the list.
+	 * Runs in O(1) time.
+	 * 
+	 * @param element - The data element to be inserted into the list.
+	 */
 	@Override
 	public void insertFirst(E element) {
 		Node<E> firstNode = new Node<E>(element, header.next);
@@ -25,7 +50,19 @@ public class SinglyLinkedList<E> implements List<E> {
 		header.next = firstNode;
 		size++;
 	}
-
+	
+	/**
+	 * Inserts the provided data element at the given index location in the list.
+	 * Data is 0-indexed, so a valid index is between 0 and the size of the list:
+	 * index = 0 corresponds to insertion at the beginning of the list,
+	 * index = size corresponds to insertion at the end of the list.
+	 * 
+	 * Runs in O(N) time.
+	 * 
+	 * @param index - the index location to insert the data element.
+	 * @param element - the data element to be inserted into the list.
+	 * @throws IndexOutOfBoundsException - if index < 0 or index > size
+	 */
 	@Override
 	public void insert(int index, E element) throws IndexOutOfBoundsException {
 		if (index < 0 || index > size) {
@@ -41,7 +78,15 @@ public class SinglyLinkedList<E> implements List<E> {
 		current.next = newElement;
 		size++;
 	}
-
+	
+	/**
+	 * Returns the data element stored in the first index in the list.
+	 * 
+	 * Runs in O(1) time.
+	 * 
+	 * @return the data element at index 0.
+	 * @throws NoSuchElementException - if there is no data stored in the list.
+	 */
 	@Override
 	public E getFirst() throws NoSuchElementException {
 		if (size == 0) {
@@ -50,6 +95,16 @@ public class SinglyLinkedList<E> implements List<E> {
 		return header.next.data;
 	}
 
+	/**
+	 * Returns the data element stored at the given index location.
+	 * Data is 0-indexed, and valid indexes are between 0 and size - 1.
+	 * 
+	 * Runs in O(N) time.
+	 * 
+	 * @param index - the index location of the data to be accessed.
+	 * @return the data element at that index.
+	 * @throws IndexOutOfBoundsException - if index < 0 or index >= size.
+	 */
 	@Override
 	public E get(int index) throws IndexOutOfBoundsException {
 		if (index < 0 || index >= size) {
@@ -64,6 +119,14 @@ public class SinglyLinkedList<E> implements List<E> {
 		return current.data;
 	}
 
+	/**
+	 * Deletes and returns the data element stored at the first index in the list.
+	 * 
+	 * Runs in O(1) time.
+	 * 
+	 * @return the deleted data element.
+	 * @throws NoSuchElementException - if there is no first element to delete.
+	 */
 	@Override
 	public E deleteFirst() throws NoSuchElementException {
 		if (size == 0) {
@@ -77,6 +140,16 @@ public class SinglyLinkedList<E> implements List<E> {
 		return temp;
 	}
 
+	/**
+	 * Deletes and returns the data element stored at the given index in the list.
+	 * Data is 0-indexed, with valid indexes between 0 and size - 1.
+	 * 
+	 * Runs in O(N) time.
+	 * 
+	 * @param index - the index location to be deleted.
+	 * @return the deleted data element.
+	 * @throws IndexOutOfBoundsException - if index < 0 or index >= size.
+	 */
 	@Override
 	public E delete(int index) throws IndexOutOfBoundsException {
 		if (index < 0 || index >= size) {
@@ -95,6 +168,16 @@ public class SinglyLinkedList<E> implements List<E> {
 		return data;
 	}
 
+	/**
+	 * Gets the index location corresponding to the first node containing a data
+	 * element equivalent to the parameter element.
+	 * If the element is not contained in the list, a value of -1 is returned instead.
+	 * 
+	 * Runs in O(N) time.
+	 * 
+	 * @param element - the data element to search for.
+	 * @return the index of the first matching node, or -1 if the element doesn't match any existing nodes.
+	 */
 	@Override
 	public int indexOf(E element) {
 		Iterator<E> iter = iterator();
@@ -111,22 +194,42 @@ public class SinglyLinkedList<E> implements List<E> {
 		return -1;
 	}
 
+	/**
+	 * Gets the number of elements contained in the list.
+	 * 
+	 * @return size of the list.
+	 */
 	@Override
 	public int size() {
 		return size;
 	}
 
+	/**
+	 * Checks if the list is currently empty, containing no data elements.
+	 * 
+	 * @return true if the list empty, false otherwise.
+	 */
 	@Override
 	public boolean isEmpty() {
 		return size == 0;
 	}
 
+	/**
+	 * Clears all data elements from the list.
+	 */
 	@Override
 	public void clear() {
 		size = 0;
 		header.next = trailer;
 	}
 
+	/**
+	 * Generates an array of type Object[] of all elements in this list in their
+	 * respective order.
+	 * The array is the same length as the number of elements in this list.
+	 * 
+	 * @return an ordered Object[] array of the elements in this list.
+	 */
 	@Override
 	public Object[] toArray() {
 		Object[] arr = new Object[size];
@@ -140,36 +243,99 @@ public class SinglyLinkedList<E> implements List<E> {
 		return arr;
 	}
 
+	/**
+	 * Generates a generic iterator for traversing this list in order.
+	 * 
+	 * @return a new iterator.
+	 */
 	@Override
 	public Iterator<E> iterator() {
 		return new SinglyLinkedListIterator();
 	}
 	
+	/**
+	 * This class represents a data node in the linked list.
+	 * It is a shell object to maintain the order of the list that contains
+	 * fields with its respective data element and a pointer to the
+	 * next node in the list.
+	 * 
+	 * @author Tyler Wilcox and Andrew Tolton
+	 * @version 28 February, 2023
+	 * 
+	 * @param <otherE> - The generic type passed into this node.
+	 */
 	private class Node<otherE>{
 		otherE data;
 		Node<otherE> next;
 		
+		/**
+		 * Creates a new node with the provided data element and a pointer to the 
+		 * next node in the list.
+		 * 
+		 * @param data - The data element stored in this node.
+		 * @param next - The next node in the list.
+		 */
 		public Node(otherE data, Node<otherE> next) {
 			this.data = data;
 			this.next = next;
 		}
 	}
 	
+	/**
+	 * This class represents an iterator for traversing SinglyLinkedList objects,
+	 * based on the Iterable<E> interface. It uses a boolean field to track if
+	 * object removal is valid and pointers to the "previous" and "current" nodes.
+	 * 
+	 * The "current" node is the last node that the iterator has seen, and is the node
+	 * that is removed when the remove() method is called.
+	 * 
+	 * The "previous" node is tracked so that removal can be done in constant time by
+	 * bypassing the "current" node and setting the previous node's pointer field to
+	 * the "next" node that the iterator would see.
+	 * 
+	 * Implements all three methods in the Iterator<E> interface, all of which
+	 * run in O(1) time.
+	 * 
+	 * @author Tyler Wilcox and Andrew Tolton
+	 * @version 28 February, 2023
+	 */
 	private class SinglyLinkedListIterator implements Iterator<E> {
 
 		boolean canRemove;
 		Node<E> previousNode;
 		
+		/**
+		 * Generates a new iterator object for SinglyLinkedList objects.
+		 */
 		public SinglyLinkedListIterator() {
 			previousNode = headerer;
 			canRemove = false;
 		}
 		
+		/**
+		 * Checks if there are any nodes remaining that this iterator can iterate over.
+		 * 
+		 * Runs in O(1) time.
+		 * 
+		 * @return true if there is a next node, false otherwise.
+		 */
 		@Override
 		public boolean hasNext() {
 			return previousNode.next.next != trailer;
 		}
 
+		/**
+		 * Iterates over the next node in this list, returning the data element
+		 * stored in that node.
+		 * This method will throw an exception if no valid nodes remain, so it is
+		 * highly encouraged to place any calls to this method within a conditional
+		 * that checks the hasNext() method.
+		 * 
+		 * Runs in O(1) time.
+		 * 
+		 * @return data stored in the traversed node.
+		 * @throws NoSuchElementException - if there is not a next node to iterate over.
+		 */
 		@Override
 		public E next() {
 			if (!hasNext()) {
@@ -183,6 +349,15 @@ public class SinglyLinkedList<E> implements List<E> {
 			return previousNode.next.data;
 		}
 		
+		/**
+		 * Removes the node that was most recently traversed by the iterator.
+		 * This method will throw an exception if it is called before the iterator
+		 * has been moved by the next() method.
+		 * 
+		 * Runs in O(1) time.
+		 * 
+		 * @throws IllegalStateException - if this method is called before the next() method.
+		 */
 		@Override
 		public void remove() {
 			if (!canRemove)
