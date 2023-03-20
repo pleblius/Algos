7b06ec7f1c053a82151192a711786503e1efc94c
Dremich <adtolton@gmail.com>
2023-03-19 23:18:01 -0600

    Remove is a b****

--- assign08/BinarySearchTree.java
+++ assign08/BinarySearchTree.java
@@ -21,6 +21,18 @@ public class BinarySearchTree<Type extends Comparable<? super Type>> implements 
 			this.parent = parent;
 		}
 		
+		public boolean hasLeftChild() {
+			return this.leftChild != null;
+		}
+		
+		public boolean hasRightChild() {
+			return this.rightChild != null;
+		}
+		
+		public boolean isLeaf() {
+			return !hasLeftChild() && !hasRightChild();
+		}
+		
 		public Node getLeftMostNode() {
 			if (this.leftChild == null)
 				return this;
@@ -84,7 +96,7 @@ public class BinarySearchTree<Type extends Comparable<? super Type>> implements 
 		
 		// Check left child, if null, add item there
 		else if (item.compareTo(current.data) < 0) {
-			if (current.leftChild == null) {
+			if (!current.hasLeftChild()) {
 				current.leftChild = new Node(item, current);
 				return true;
 			}
@@ -94,7 +106,7 @@ public class BinarySearchTree<Type extends Comparable<? super Type>> implements 
 			
 		// Check right child, if null, add item there
 		else {
-			if (current.rightChild == null) {
+			if (!current.hasRightChild()) {
 				current.rightChild = new Node(item, current);
 				return true;
 			}
@@ -133,14 +145,14 @@ public class BinarySearchTree<Type extends Comparable<? super Type>> implements 
 			return true;
 		}
 		else if(item.compareTo(current.data) < 0) {
-			if (current.leftChild == null) {
+			if (!current.hasLeftChild()) {
 				return false;
 			}
 			
 			return contains(item, current.leftChild);
 		}
 		else {
-			if (current.rightChild == null) {
+			if (!current.hasRightChild()) {
 				return false;
 			}
 			
@@ -186,6 +198,17 @@ public class BinarySearchTree<Type extends Comparable<? super Type>> implements 
 	@Override
 	public boolean remove(Type item) {
 		if (size == 0) return false;
+		if (size == 1) {
+			if (root.data == item) {
+				root.data = null;
+				size--;
+				return true;
+			}
+			
+			else {
+				return false;
+			}
+		}
 		
 		boolean didRemove = false;
 		didRemove = remove(item, root);
@@ -197,7 +220,44 @@ public class BinarySearchTree<Type extends Comparable<? super Type>> implements 
 	}
 	
 	private boolean remove(Type item, Node current) {
+		Node toRemove;
+		
+		if (current.isLeaf()) {
+			return false;
+		}
+		
+		else if (current.leftChild.equals(item)) {
+			return killChild(current, current.leftChild);
+		}
+		else if (current.rightChild.equals(item)) {
+			return killChild(current, current.rightChild);
+		}
+		
+		else if (item.compareTo(current.data) < 0) {
+			return remove(item, current.leftChild);
+		}
+		
+		else {
+			return remove(item, current.rightChild);
+		}
+		
 	}
+	
+	private boolean killChild(Node parent, Node toRemove) {
+		
+		// One-child node
+		
+		parent.child = toRemove.child
+		
+		// Two-child node
+		parent.leftChild = toRemove.getPredecessor()
+			// predesessor = null
+		parent.rightChild = toRemove.getSuccessor()
+			// successor = null
+		
+				
+	}
+
 
 	@Override
 	public boolean removeAll(Collection<? extends Type> items) {
