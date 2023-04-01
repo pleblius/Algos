a3a523d7b4b099593216ae77dbc4da02ab694f83
pleblius <pleblius@gmail.com>
2023-03-31 21:15:32 -0600

    Assignment 09 initial files

---
+++ assign09/HashTable.java
@@ -0,0 +1,61 @@
+package assign09;
+
+import java.util.List;
+
+public class HashTable<K, V> implements Map<K, V> {
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean containsKey(K key) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsValue(V value) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<MapEntry<K, V>> entries() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public V get(K key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public V put(K key, V value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public V remove(K key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
