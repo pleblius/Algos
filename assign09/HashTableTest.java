44f62af9c0fd475995d8fde5aecd5a7c008bf655
Dremich <adtolton@gmail.com>
2023-04-02 18:40:37 -0600

    Testing begins!

--- assign09/HashTableTest.java
+++ assign09/HashTableTest.java
@@ -2,63 +2,137 @@ package assign09;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class HashTableTest {
 
+	ArrayList<String> stringKeys;
+	ArrayList<Integer> intKeys;
+	
+	ArrayList<String> stringVals;
+	ArrayList<Integer> intVals;
+	
+	HashTable<String, Integer> stringInt;
+	HashTable<String, String> stringString;
+	HashTable<Integer, String> intString;
+	HashTable<Integer, Integer> intInt;
+	HashTable<Integer, Integer> empty;
+	
 	@BeforeEach
 	void setUp() {
-	}
-
-	@Test
-	void testHashTable() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testClear() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testContainsKey() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testContainsValue() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testEntries() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGet() {
-		fail("Not yet implemented");
+		stringKeys = new ArrayList<String>();
+		stringVals = new ArrayList<String>();
+		
+		intKeys = new ArrayList<Integer>();
+		intVals = new ArrayList<Integer>();
+		
+		stringInt = new HashTable<String, Integer>();
+		stringString = new HashTable<String, String>();
+		intString = new HashTable<Integer, String>();
+		intInt = new HashTable<Integer, Integer>();
+		empty = new HashTable<Integer, Integer>();
+		
+		
+		for (int ii = 0; ii < 100; ii++) {
+			stringKeys.add(Character.toChars(ii).toString());
+			stringVals.add(Character.toChars(100-ii).toString());
+			
+			intKeys.add(ii);
+			intVals.add(100 - ii);
+			
+			stringInt.put(stringKeys.get(ii), intVals.get(ii));
+			stringString.put(stringKeys.get(ii), stringVals.get(ii));
+			intString.put(intKeys.get(ii), stringVals.get(ii));
+			intInt.put(intKeys.get(ii), intVals.get(ii));
+		}		
+		
 	}
 
 	@Test
 	void testIsEmpty() {
-		fail("Not yet implemented");
+		assertTrue(empty.isEmpty());
+		empty.put(1, 1);
+		assertFalse(empty.isEmpty());
+		
+		assertFalse(stringInt.isEmpty());
+	}
+	
+	@Test
+	void emptyClear() {
+		empty.clear();
+		assertTrue(empty.isEmpty());
+	}
+	
+	@Test
+	void testClear() {
+		stringString.clear();
+		assertTrue(stringString.isEmpty());	
+		assertTrue(stringString.entries().isEmpty());
+	}
+	
+	@Test
+	void testClearAddClear() {
+		assertFalse(intString.isEmpty());
+		intString.clear();
+		assertTrue(intString.isEmpty());
+		intString.put(1, "hi!");
+		assertFalse(intString.isEmpty());
+	}
+	
+	@Test
+	void testGet() {
+		
+		for (int ii = 0; ii < 100; ii++) {
+			assertEquals(100 - ii, intInt.get(ii));
+		}
+
+	}
+	
+	@Test
+	void testGetAfterClear() {
+		intInt.clear();
+		
+		assertEquals(null, intInt.get(10));
 	}
 
 	@Test
-	void testPut() {
-		fail("Not yet implemented");
+	void testContainsKey() {
+		for (int ii = 0; ii < 100; ii++) {
+			assertTrue(intInt.containsKey(ii));
+		}
 	}
-
+	
 	@Test
 	void testRemove() {
-		fail("Not yet implemented");
+		assertTrue(intInt.containsKey(10));
+		intInt.remove(10);
+		assertFalse(intInt.containsKey(10));
 	}
-
+	
 	@Test
-	void testSize() {
-		fail("Not yet implemented");
+	void testContainsAfterRemove() {
+		
 	}
-
+//
+//	@Test
+//	void testContainsValue() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	void testEntries() {
+//		fail("Not yet implemented");
+//	}
+//
+//
+//
+//
+//	@Test
+//	void testSize() {
+//		fail("Not yet implemented");
+//	}
+//
 }
