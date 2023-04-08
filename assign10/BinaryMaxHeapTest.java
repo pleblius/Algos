e1a38260d6db5d2c664d1d08d84dcd2abcfd19b0
pleblius <pleblius@gmail.com>
2023-04-08 12:34:11 -0600

    Added testing files

---
+++ assign10/BinaryMaxHeapTest.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package assign10;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test cases for the BinaryMaxHeap class
+ * 
+ * @author Tyler Wilcox and Andrew Tolton
+ * @version 08 April, 2023
+ */
+class BinaryMaxHeapTest {
+
+	
+	
+	@BeforeEach
+	void setUp() {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ assign10/FindKLargestTest.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package assign10;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test cases for the FindKLargest class
+ * 
+ * @author Tyler Wilcox and Andrew Tolton
+ * @version 08 April, 2023
+ */
+class FindKLargestTest {
+
+	
+	
+	@BeforeEach
+	void setUp() {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
